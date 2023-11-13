fb2d4f0387e6ea52caafcac6629081e45b4eb167
Dragon-Heath56 <143317198+Dragon-Heath56@users.noreply.github.com>
2023-11-13 16:28:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
     raccoonImage.style.filter = `hue-rotate(${hueRotation}deg)`;
   });
 });
+
+document.addEventListener('keydown', function(event) {
+  // Check if Ctrl and Alt are pressed along with the 'L' key
+  if (event.ctrlKey && event.altKey && event.key === 'l') {
+    // Prevent the default action to avoid triggering any browser shortcuts
+    event.preventDefault();
+
+    // Your login code here, for example, showing a login prompt
+    var username = window.prompt("Username:", "");
+    var password = window.prompt("Password:", "");
+
+    if(username === "Raccoon" && password === "Panda") {
+      alert("I'm still broken inside. I hate you for what you've done to me.");
+    } else {
+      alert("Wrong. You aren't the person meant to be logging in either.");
+    }
+
+    // You might want to send this information to your server
+    // Use AJAX or a form submission to do this securely
+  }
+});
+
