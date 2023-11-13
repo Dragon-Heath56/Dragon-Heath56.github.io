b53d902d3d122ba99933bf0dd114b1a39ab28d5c
Dragon-Heath56 <143317198+Dragon-Heath56@users.noreply.github.com>
2023-11-13 13:46:14 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // script.js
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    const raccoonImage = document.getElementById('raccoonImage');
-    raccoonImage.addEventListener('click', () => {
-      raccoonImage.style.filter = 'hue-rotate(90deg)';
-    });
+  const raccoonImage = document.getElementById('raccoonImage');
+  let hueRotation = 0; // Starting hue rotation value
+
+  raccoonImage.addEventListener('click', () => {
+    hueRotation += 90; // Increase hue rotation by 90 degrees on each click
+    if (hueRotation >= 360) { // If hue rotation completes a full circle,
+      hueRotation = 0; // reset it to start over.
+    }
+    raccoonImage.style.filter = `hue-rotate(${hueRotation}deg)`;
   });
-  
+});
